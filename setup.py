eef48d46f1c3696644523e0b108d9d213cd2b10e
= <=>
2022-01-06 14:03:59 +0000

    Version bumps

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
 import setuptools
 
-setuptools.setup(setup_requires=["pbr>=2.0.0"], pbr=True)
+
+with open("requirements.txt", "r") as fp:
+    required = fp.read().splitlines()
+
+setuptools.setup(
+    name="online-neural-cdes",
+    version="0.0.3",
+    author="James Morrill",
+    author_email="james.morrill.6@gmail.com",
+    description="Online neural CDEs paper",
+    url="https://github.com/jambo6/online-neural-cdes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=required,
+    extras_require={
+        "test": ["pytest"]
+    }
+)
